22f4882f4bed0cb6e1e84bc92c72a5b148807b76
Bekir Berat Kamacı <bkamaci@protel.com.tr>
2023-10-23 19:13:52 +0300

    refactor

---
+++ RabbitMQ_Direct_Exchange_Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+// Create connection 
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://xgtppcak:9E7tafT5XUlGRnKeOPZTnMn1L4-OtIvE@jackal.rmq.cloudamqp.com/xgtppcak");
+
+// Activate connection and create channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Declare queue
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// Send message to queue
+for (int i = 0; i < 100; i++)
+{
+    Task.Delay(100).Wait();
+    byte[] message = Encoding.UTF8.GetBytes($"{i}. Hello World");
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+Console.Read();
---
+++ RabbitMq_Direct_Exchange_Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿// Create connection 
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Create connection 
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://xgtppcak:9E7tafT5XUlGRnKeOPZTnMn1L4-OtIvE@jackal.rmq.cloudamqp.com/xgtppcak");
+
+// Activate connection and create channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Declare queue
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// Read message from queue
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: true, consumer: consumer);
+consumer.Received += (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+Console.Read();
