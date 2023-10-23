be6ff9b01116f971d5ec9769f3c2238abdfe3e46
Bekir Berat Kamacı <bkamaci@protel.com.tr>
2023-10-23 21:03:58 +0300

    Topic Exchange

---
+++ RabbitMQ_Topic_Exchange_Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿// Create connection 
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Create connection 
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://xgtppcak:9E7tafT5XUlGRnKeOPZTnMn1L4-OtIvE@jackal.rmq.cloudamqp.com/xgtppcak");
+
+// Activate connection and create channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Declare exchange
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+// Take topic parameter from user
+Console.WriteLine("Enter publisher topic: ");
+string topic = Console.ReadLine();
+
+// Send message
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(500);
+    string message = $"Message {i}";
+    byte[] body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "topic-exchange-example", routingKey: topic, body: body);
+}
+
+Console.Read();
---
+++ RabbitMq_Topic_Exchange_Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿// Create connection 
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Create connection 
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://xgtppcak:9E7tafT5XUlGRnKeOPZTnMn1L4-OtIvE@jackal.rmq.cloudamqp.com/xgtppcak");
+
+// Activate connection and create channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Declare exchange
+channel.ExchangeDeclare(exchange: "topic-exchange-example", type: ExchangeType.Topic);
+
+// Take topic parameter from user
+Console.WriteLine("Enter consumer topic: ");
+string topic = Console.ReadLine();
+
+// Declare queue
+string queueName = channel.QueueDeclare().QueueName;
+
+// Bind queue to exchange
+channel.QueueBind(queue: queueName, exchange: "topic-exchange-example", routingKey: topic);
+
+// Read message
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    byte[] body = e.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message received: {message}");
+};
+
+
+Console.Read();
