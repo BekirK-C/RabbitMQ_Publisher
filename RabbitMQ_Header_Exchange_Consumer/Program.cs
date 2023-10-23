e3f11c7aca7eebae81cf0c2430cc1b48eebfbb9c
Bekir Berat Kamacı <b.b.kamac@gmail.com>
2023-10-23 21:40:22 +0300

    header exchange

---
+++ RabbitMQ_Header_Exchange_Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿// Create connection 
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Create connection 
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://xgtppcak:9E7tafT5XUlGRnKeOPZTnMn1L4-OtIvE@jackal.rmq.cloudamqp.com/xgtppcak");
+
+// Activate connection and create channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Declare exchange
+channel.ExchangeDeclare(exchange: "header-exchange-example", type: ExchangeType.Headers);
+
+// Take header parameters from user
+Console.WriteLine("Enter consumer header parameters: ");
+string value = Console.ReadLine()!;
+
+// Create queue
+string queueName = channel.QueueDeclare().QueueName;
+
+// Create binding
+channel.QueueBind(queue: queueName, exchange: "header-exchange-example", routingKey: string.Empty, arguments: new Dictionary<string, object>()
+{
+    ["yes"] = value
+});
+
+// Create consumer
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+// Receive message
+consumer.Received += (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ_Header_Exchange_Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿// Create connection 
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Create connection 
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://xgtppcak:9E7tafT5XUlGRnKeOPZTnMn1L4-OtIvE@jackal.rmq.cloudamqp.com/xgtppcak");
+
+// Activate connection and create channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Create Exchange
+channel.ExchangeDeclare(exchange: "header-exchange-example", type: ExchangeType.Headers);
+
+// Take header parameters from user
+Console.WriteLine("Enter publisher header parameters: ");
+string value = Console.ReadLine()!;
+
+// Create properties
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Headers = new Dictionary<string, object>()
+{
+    ["yes"] = value
+};
+
+// Create message
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(500);
+    string message = $"Message {i}";
+    byte[] body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "header-exchange-example", routingKey: string.Empty, body: body);
+}
+
+Console.Read();
