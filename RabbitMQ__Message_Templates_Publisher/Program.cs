05b57afa47062ec94da4b02c0d4098999baa6d01
Bekir Berat Kamacı <b.b.kamac@gmail.com>
2023-10-25 00:16:08 +0300

    Message Templates

---
+++ RabbitMQ__Message_Templates_Publisher/Program.cs
@@ -0,0 +1,99 @@
+﻿// Create connection 
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Create connection 
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://xgtppcak:9E7tafT5XUlGRnKeOPZTnMn1L4-OtIvE@jackal.rmq.cloudamqp.com/xgtppcak");
+
+// Activate connection and create channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#region P2P (Point-to-Point) Template
+
+//// Declare queue
+//var queueName = "P2P_Queue";
+//channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false);
+
+//// Create message
+//byte[] body = Encoding.UTF8.GetBytes("Hello World!");
+//channel.BasicPublish(exchange: string.Empty, routingKey: queueName, body: body);
+
+#endregion
+
+#region Publish/Subscribe (Pub/Sub) Template
+
+//// Declare exchange
+//var exchangeName = "PubSub_Exchange";
+//channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+
+//// Create message
+//for (int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(300);
+//    byte[] body2 = Encoding.UTF8.GetBytes($"Hello World! {i}");
+//    channel.BasicPublish(exchange: exchangeName, routingKey: string.Empty, body: body2);
+//}
+
+#endregion
+
+#region Work Queue Template
+
+//// Declare queue
+//string queueName3 = "WorkQueue_Queue";
+//channel.QueueDeclare(queue: queueName3, durable: false, exclusive: false, autoDelete: false);
+
+//// Create message
+//for (int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(300);
+//    byte[] body3 = Encoding.UTF8.GetBytes($"Hello World! {i}");
+//    channel.BasicPublish(exchange: string.Empty, routingKey: queueName3, body: body3);
+//}
+
+#endregion
+
+#region Request/Response Template
+
+// Declare request queue 
+var requestQueueName = "request-response-queueu";
+channel.QueueDeclare(queue: requestQueueName, durable: false, exclusive: false, autoDelete: false);
+
+// Declare response queue
+var responseQueueName = channel.QueueDeclare().QueueName;
+
+// Corelation ID
+var correlationId = Guid.NewGuid().ToString();
+
+// Create properties
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.ReplyTo = responseQueueName;
+properties.CorrelationId = correlationId;
+
+// Create message
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(300);
+    byte[] body4 = Encoding.UTF8.GetBytes($"Hello World! {i}");
+    channel.BasicPublish(exchange: string.Empty, routingKey: requestQueueName, basicProperties: properties, body: body4);
+}
+
+// Read response
+var consumer4 = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: responseQueueName, autoAck: true, consumer: consumer4);
+consumer4.Received += (sender, e) =>
+{
+    if (e.BasicProperties.CorrelationId == correlationId)
+    {
+        var body = e.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"Message received: {message}");
+    }
+};
+
+#endregion
+
+
+Console.Read();
---
+++ RabbitMq_Message_Templates_Consumer/Program.cs
@@ -0,0 +1,111 @@
+﻿// Create connection 
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Create connection 
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://xgtppcak:9E7tafT5XUlGRnKeOPZTnMn1L4-OtIvE@jackal.rmq.cloudamqp.com/xgtppcak");
+
+// Activate connection and create channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#region P2P (Point-to-Point) Template
+
+//// Declare queue
+//var queueName = "P2P_Queue";
+//channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false);
+
+//// Read message
+//var consumer = new EventingBasicConsumer(channel);
+//channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+//consumer.Received += (sender, e) =>
+//{
+//    var body = e.Body.ToArray();
+//    var message = Encoding.UTF8.GetString(body);
+//    Console.WriteLine($"Message received: {message}");
+//};
+
+#endregion
+
+#region Publish/Subscribe (Pub/Sub) Template
+
+//// Declare exchange
+//var exchangeName = "PubSub_Exchange";
+//channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+
+//// Create queue
+//var queueName2 = channel.QueueDeclare().QueueName;
+
+//// Bind queue to exchange
+//channel.QueueBind(queue: queueName2, exchange: exchangeName, routingKey: string.Empty);
+
+//// Read message
+//var consumer2 = new EventingBasicConsumer(channel);
+//channel.BasicConsume(queue: queueName2, autoAck: true, consumer: consumer2);
+//consumer2.Received += (sender, e) =>
+//{
+//    var body = e.Body.ToArray();
+//    var message = Encoding.UTF8.GetString(body);
+//    Console.WriteLine($"Message received: {message}");
+//};
+
+#endregion
+
+#region Work Queue Template
+
+//// Declare queue
+//string queueName3 = "WorkQueue_Queue";
+//channel.QueueDeclare(queue: queueName3, durable: false, exclusive: false, autoDelete: false);
+
+//// scaling
+//channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+//// Read message
+//var consumer3 = new EventingBasicConsumer(channel);
+//channel.BasicConsume(queue: queueName3, autoAck: true, consumer: consumer3);
+//consumer3.Received += (sender, e) =>
+//{
+//    var body = e.Body.ToArray();
+//    var message = Encoding.UTF8.GetString(body);
+//    Console.WriteLine($"Message received: {message}");
+//};
+
+#endregion
+
+#region Request/Response Template
+
+// Declare request queue 
+var requestQueueName = "request-response-queueu";
+channel.QueueDeclare(queue: requestQueueName, durable: false, exclusive: false, autoDelete: false);
+
+// Read message
+var consumer4 = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: requestQueueName, autoAck: true, consumer: consumer4);
+consumer4.Received += (sender, e) =>
+{
+    var body = e.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message received: {message}");
+
+    // Declare response queue
+    var responseQueueName = channel.QueueDeclare().QueueName;
+
+    // Corelation ID
+    var correlationId = e.BasicProperties.CorrelationId;
+
+    // Create properties
+    IBasicProperties properties = channel.CreateBasicProperties();
+    properties.ReplyTo = responseQueueName;
+    properties.CorrelationId = correlationId;
+
+    // Create response message
+    byte[] body4 = Encoding.UTF8.GetBytes($"Mission Completed! {message}");
+    channel.BasicPublish(exchange: string.Empty, routingKey: responseQueueName, basicProperties: properties, body: body4);
+};
+
+#endregion
+
+
+Console.Read();
